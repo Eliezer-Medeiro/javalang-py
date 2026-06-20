b970e2129e02cc8e93bf3714693ee0e0dca80e2d
mariajustino <mjustinorodrigues94@gmail.com>
2026-06-20 17:56:58 -0400

    feat: implementar construtores e tratamento de string no JFloat refs #21

---
+++ javalang/jfloat.py
@@ -0,0 +1,13 @@
+from typing import Final, Type, Union
+
+
+class JFloat:
+    # Construtor da classe JFloat
+    def __init__(self, value: Union[float, int, str] = 0.0):
+        if isinstance(value, str):
+            try:
+                self.value = float(value)
+            except ValueError as e:
+                raise ValueError(f"Invalid string for JFloat: '{value}'") from e
+        else:
+            self.value = float(value)
